d0f6f038f2dca1e121077a7592e0dd2240c1a4f6
wils0n <wiljm0.0@gmail.com>
2015-10-05 00:01:38 -0500

    using model mommy

--- whatever/tests.py
+++ whatever/tests.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 from django.utils import timezone
 
+from model_mommy import mommy
+
 from .models import Whatever
 
 # models test
-
+"""
 class WhateverTest(TestCase):
 
     def create_whatever(self, title="only a test", body="yes, this is only a test"):
@@ -14,3 +16,12 @@ class WhateverTest(TestCase):
         w = self.create_whatever()
         self.assertTrue(isinstance(w, Whatever))
         self.assertEqual(w.__unicode__(), w.title)
+"""
+
+
+class WhateverTestMommy(TestCase):
+
+    def test_whatever_creation_mommy(self):
+        what = mommy.make(Whatever)
+        self.assertTrue(isinstance(what, Whatever))
+        self.assertEqual(what.__unicode__(), what.title)
