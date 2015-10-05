031ecaee4ad2d500bfeac7d1abfa83ec12c15999
wils0n <wiljm0.0@gmail.com>
2015-10-04 22:15:55 -0500

    unit test model

--- whatever/tests.py
+++ whatever/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Whatever
+
+# models test
+
+class WhateverTest(TestCase):
+
+    def create_whatever(self, title="only a test", body="yes, this is only a test"):
+        test_whatever = Whatever.objects.create(title=title, body=body, created_at=timezone.now())
+
+    def test_whatever_creation(self):
+        w = self.create_whatever()
+        self.assertTrue(isinstance(w, Whatever))
+        self.assetEqual(w.__unicode__, w.title)
